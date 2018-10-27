97968535ebcf2f29c1dcc9b731e293411145e5cc
marek.hradil <marek.hradil@staff.techfides.eu>
2018-10-28 00:19:57 +0200

    Reworked modals

--- Components/Courses/BackendCourse.tsx
+++ Components/Courses/BackendCourse.tsx
@@ -3,51 +3,98 @@ import Typography from '@material-ui/core/Typography'
 import * as React from 'react'
 import PointList from './PointList'
 import Button from '@material-ui/core/Button'
+import Hidden from '@material-ui/core/Hidden'
+import Grid from '@material-ui/core/Grid'
+import Avatar from '@material-ui/core/Avatar'
 
 const cardMediaStyle = {
-  padding: '70px 0',
-  textAlign: 'center',
+  padding: '5rem 0',
+  textAlign: 'center' as 'center',
 }
 interface IProps {
   classes?: any
 }
-export default ({ classes }: IProps) => (
+export default ({ classes }: IProps) => (
   <div>
+    <Typography
+      style={{
+        marginBottom: '1rem',
+        textTransform: 'uppercase',
+        fontSize: '0.9rem'
+      }}
+    >
+      Node backend rest-api/GraphQL Backend
+    </Typography>
     <CardMedia
       image={'static/img/courses/nodeBackend.png'}
-      style={cardMediaStyle}
       title='Contemplative Reptile'
+      style={cardMediaStyle}
     />
     <br />
-    <Typography variant='h4' id='modal-title'> style={{ marginBottom: '20px' }}
-      Kdo jsme?
-    </Typography>
-    <Typography style={{ marginBottom: '20px' }}>
+    <Typography style={{ fontSize: '1rem', padding: '1rem 5vw' }}>
       Jsme skupina lidí, kteri si v životě prošli práci na enterprise projektech a chtějí
       zachránit lidstvo a vzbouřit programový k tomu, ať se do starého enterprise
       kodu už nový nepřidává a věci se upravují a budují znova a čištěji
     </Typography>
 
+    <Grid container spacing={24}>
+      <Hidden xsDown>
+        <Grid
+          item
+          md={5}
+          sm={5}
+          style={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+        >
+          <Avatar
+            alt='lecturer'
+            src='../../../static/img/ourTeam/svehla.jpg'
+            style={{ width: '25vh', height: '25vh', margin: '10vh 0' }}
+          />
+        </Grid>
+      </Hidden>
+      <Grid
+        item
+        md={6}
+        sm={5}
+        xs={12}
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          flexDirection: 'column',
+          margin: '1.5rem 0'
+        }}
+      >
+        Školící
+        <Typography variant='h5' style={{ marginBottom: '0.5rem' }}>
+          Švehl Jakub
+        </Typography>
+        <Typography>
+          Za poslední roky jsem prošel mnoho projektů od server render jade šablon v nodejs,
+          až k relay graphql stacku podobnému tomu, který používá a vývíjí například facebook.com
+        </Typography>
+      </Grid>
+    </Grid>
 
-    <Typography style={{ marginBottom: '20px' }}>
-      Technologie které skolime využívají statisíce firem...
-      Například Facebook airbnb github apple a milion dalších firema
-      pomáhají jim k ladnemu chodu.
+    <Typography
+      variant='h5'
+      style={{
+        marginLeft: '1rem',
+        textAlign: 'center'
+      }}
+    >
+      Kurz obnáší
     </Typography>
-
-    <Typography style={{ marginBottom: '20px' }}>
-      Všechny technologie které učíme lze integrovat
-      do vašeho již rozděleného systému a nemusíte se
-      bát zpětné nekompatibily
-    </Typography>
-
-    <Typography variant='h5'>Kurz obnáší</Typography>
     <PointList
       dataList={[
         'Objevení ekosystému pro nodejs',
         'Architektura nodejs backend aplikací',
         'Vytvoříme jednoduchou CRUD (Create/Read/Update/Delete)' +
-          'REST-API aplikaci pomocí express.js',
+        'REST-API aplikaci pomocí express.js',
         'Představení graphQL',
         'Ukážeme si, proč GraphQL je vítěz na poli dnešních webových API',
         'Napišeme si vlastní GraphQL server',
@@ -62,21 +109,26 @@ export default ({ classes }: IProps) => (
       ]}
     />
 
-
-
-    <Typography variant='h5'>Co budete potřebovat?</Typography>
+    <Typography
+      variant='h5'
+      style={{
+        marginLeft: '1rem',
+        textAlign: 'center'
+      }}
+    >
+      Co budete potřebovat?
+    </Typography>
     <PointList
       dataList={[
         `Elementární znalost programování`,
-        `Vlastní notebook`,
+        `Vlastní notebook`
       ]}
     />
-    <Button variant='contained' color='primary'>
+    <Button variant='contained' color='primary' style={{ marginRight: '1rem' }}>
       Objednat
     </Button>
-    <Button variant='contained' color='action' >
+    <Button variant='contained'>
       Zavřít
     </Button>
-
   </div>
 )
--- Components/Courses/FrontendAdvanced.tsx
+++ Components/Courses/FrontendAdvanced.tsx
@@ -3,32 +3,92 @@ import Typography from '@material-ui/core/Typography'
 import * as React from 'react'
 import PointList from './PointList'
 import Button from '@material-ui/core/Button'
+import Avatar from '@material-ui/core/Avatar'
+import Grid from '@material-ui/core/Grid'
+import Hidden from '@material-ui/core/Hidden'
 
 const cardMediaStyle = {
-  padding: '70px 0',
+  padding: '7rem',
   textAlign: 'center' as 'center',
 }
 interface IProps {
   classes?: any
 }
-export default ({ classes }: IProps) => (
+export default ({ classes }: IProps) => (
   <div>
+    <Typography
+      style={{
+        marginBottom: '1rem',
+        textTransform: 'uppercase',
+        fontSize: '0.9rem'
+      }}
+    >
+      REACT GRAPHQL NINJA
+    </Typography>
     <CardMedia
       image={'static/img/courses/imageFrontendAdvanced.png'}
       title='Contemplative Reptile'
       style={cardMediaStyle}
     />
     <br />
-    <Typography variant='h4' id='modal-title'>
-      React GraphQL ninja
-    </Typography>
-    <Typography style={{ marginBottom: '20px' }}>
+    <Typography style={{ fontSize: '1rem', padding: '1rem 5vw' }}>
       Máte už zkušenosti s REST-API i reactem a přestává vás bavit
       monotonní práce psaní nudných redux storů a chcete se zase cítit jako v době,
       kdy jste spustili svůj první for cyklus, nebo dali do produkce první aplikaci?
     </Typography>
 
-    <Typography variant='h5'>Kurz obnáší</Typography>
+    <Grid container spacing={24}>
+      <Hidden xsDown>
+        <Grid
+          item
+          md={5}
+          sm={5}
+          style={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+        >
+          <Avatar
+            alt='lecturer'
+            src='../../../static/img/ourTeam/svehla.jpg'
+            style={{ width: '25vh', height: '25vh', margin: '10vh 0' }}
+          />
+        </Grid>
+      </Hidden>
+      <Grid
+        item
+        md={6}
+        sm={5}
+        xs={12}
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          flexDirection: 'column',
+          margin: '1.5rem 0'
+        }}
+      >
+        Školící
+        <Typography variant='h5' style={{ marginBottom: '0.5rem' }}>
+          Švehl Jakub
+        </Typography>
+        <Typography>
+          Za poslední roky jsem prošel mnoho projektů od server render jade šablon v nodejs,
+          až k relay graphql stacku podobnému tomu, který používá a vývíjí například facebook.com
+        </Typography>
+      </Grid>
+    </Grid>
+
+    <Typography
+      variant='h5'
+      style={{
+        marginLeft: '1rem',
+        textAlign: 'center'
+      }}
+    >
+      Kurz obnáší
+    </Typography>
     <PointList
       dataList={[
         'Představení moderních SPA aplikací a frameworků (zaměření na GraphQL)',
@@ -43,7 +103,15 @@ export default ({ classes }: IProps) => (
       ]}
     />
 
-    <Typography  variant='h5'>CSS stylování a design</Typography>
+    <Typography
+      variant='h5'
+      style={{
+        marginLeft: '1rem',
+        textAlign: 'center'
+      }}
+    >
+      CSS stylování a design
+    </Typography>
     <PointList
       dataList={[
         'Představíme si nejmoderněší přístup ke stylování: CSS in JS',
@@ -54,7 +122,15 @@ export default ({ classes }: IProps) => (
     />
 
 
-    <Typography variant='h5'>Co budete potřebovat?</Typography>
+    <Typography
+      variant='h5'
+      style={{
+        marginLeft: '1rem',
+        textAlign: 'center'
+      }}
+    >
+      Co budete potřebovat?
+    </Typography>
     <PointList
       dataList={[
         'Základní znalost javascriptu',
@@ -63,13 +139,11 @@ export default ({ classes }: IProps) => (
         `Vlastní notebook`
       ]}
     />
-    <Button variant='contained' color='primary'>
+    <Button variant='contained' color='primary' style={{ marginRight: '1rem' }}>
       Objednat
     </Button>
-    <Button variant='contained' color='action' >
+    <Button variant='contained'>
       Zavřít
     </Button>
-
-
   </div>
 )
